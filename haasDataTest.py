d370bdf1ff96a01633814583788afad93f8f6122
Santiago <ssant96@vt.edu>
2023-05-18 15:11:23 -0400

    fixed the code crashing when Haas switches program

--- haasDataTest.py
+++ haasDataTest.py
@@ -1,23 +1,43 @@
 import serial
 import time
-import datetime
 
 """Function that reads data from serial (through Rasp. Pi) and parses the data"""
 
 def readData(ser, HAASCode):
     try:
+        print("Inside readData loop...")
         ser.write(bytes("?Q600 " + HAASCode + "\r\n", "ascii"))
+        print("Query ?Q600 made...")
+        count = 0
+        retry_limit = 10  # Limit of retries
+        retries = 0       # Initial retry count
+
         while True:
+            count += 1
+            print("Inside while loop")
             value = ser.readline().decode("utf-8").strip()
+            print(f"Value decoded {count} and value is [{value}]")
+
             if len(value) > 4:
+                print("broke")
                 break
+            
+            # Increment retries count and check if limit is reached
+            retries += 1
+            if retries >= retry_limit:
+                print("Retry limit reached.")
+                return "Unavailable"
+
         value = value.split(",")[2].strip()
+        print(f"Value was split {count} and splitValue is [{value}]")
         value = value.replace(chr(23), '')
+        print(f"Value was replaced {count} and replaceValue is [{value}]")
     except Exception as ex:
         print(ex)
         value = 'Unavailable'
     return value
 
+#----------------------------------------------------------------------------------------#
 
 def fetch_from_Haas():
         # Create serial object
@@ -33,38 +53,13 @@ def fetch_from_Haas():
         time.sleep(1)
 
         # Init of values to update
-        coolantPrevious = "novalue"
-        spindleSpeedPrevious = "novalue"
         xMachinePrevious = "novalue"
-        yMachinePrevious = "novalue"
-        zMachinePrevious = "novalue"
-        aMachinePrevious = "novalue"
-        bMachinePrevious = "novalue"
-        xWorkPrevious = "novalue"
-        yWorkPrevious = "novalue"
-        zWorkPrevious = "novalue"
-        aWorkPrevious = "novalue"
-        bWorkPrevious = "novalue"
 
         while True:
             updated = False
             try:
                 # Combines all output into one at the end
                 outString = ""
-
-                # Coolant
-                coolant = readData(ser, "1094")
-                if coolant != coolantPrevious:
-                    outString += "|coolant|"+coolant
-                    coolantPrevious = coolant
-                print(f"coolant: {coolant}")
-
-                # Spindle Speed
-                spindleSpeed = readData(ser, "3027")
-                if spindleSpeed != spindleSpeedPrevious:
-                    outString += "|spindleSpeed|"+spindleSpeed
-                    spindleSpeedPrevious = spindleSpeed
-                print(f"spindleSpeed: {spindleSpeed}")
 
                 # X machine
                 xMachine = readData(ser, "5021")
@@ -73,82 +68,13 @@ def fetch_from_Haas():
                     xMachinePrevious = xMachine
                 print(f"xMachine: {xMachine}")
 
-                # Y machine
-                yMachine = readData(ser, "5022")
-                if yMachine != yMachinePrevious:
-                    outString += "|yMachine|"+yMachine
-                    yMachinePrevious = yMachine
-                print(f"yMachine: {yMachine}")
-
-                # Z machine
-                zMachine = readData(ser, "5023")
-                if zMachine != zMachinePrevious:
-                    outString += "|zMachine|"+zMachine
-                    zMachinePrevious = zMachine
-                print(f"zMachine: {zMachine}")
-
-                # A machine
-                aMachine = readData(ser, "5024")
-                if aMachine != aMachinePrevious:
-                    outString += "|aMachine|"+aMachine
-                    aMachinePrevious = aMachine
-                print(f"aMachine: {aMachine}")
-
-                # B machine
-                bMachine = readData(ser, "5025")
-                if bMachine != bMachinePrevious:
-                    outString += "|bMachine|"+bMachine
-                    bMachinePrevious = bMachine
-                print(f"bMachine: {bMachine}")
-
-                # X work
-                xWork = readData(ser, "5041")
-                if xWork != xWorkPrevious:
-                    outString += "|xWork|"+xWork
-                    xWorkPrevious = xWork
-                print(f"xWork: {xWork}")
-
-                # Y work
-                yWork = readData(ser, "5042")
-                if yWork != yWorkPrevious:
-                    outString += "|yWork|"+yWork
-                    yWorkPrevious = yWork
-                print(f"yWork: {yWork}")
-
-                # Z work
-                zWork = readData(ser, "5043")
-                if zWork != zWorkPrevious:
-                    outString += "|zWork|"+zWork
-                    zWorkPrevious = zWork
-                print(f"zWork: {zWork}")
-
-                # A work
-                aWork = readData(ser, "5044")
-                if aWork != aWorkPrevious:
-                    outString += "|aWork|"+aWork
-                    aWorkPrevious = aWork
-                print(f"aWork: {aWork}")
-
-                # B work
-                bWork = readData(ser, "5045")
-                if bWork != bWorkPrevious:
-                    outString += "|bWork|"+bWork
-                    bWorkPrevious = bWork
-                print(f"bWork: {bWork}")
-
-                # Final data purge
-                combined_output = '\r\n' + datetime.datetime.now().isoformat() + 'Z' + outString
-                print(f"------> {combined_output}")
+                print(f"--------------{outString}--------------")
 
             # Error catch
             except Exception as ex:
                 print("Failed fetching values from machine: ")
                 print(ex)
                 time.sleep(2)
-
-            
-        ser.close()
-
 fetch_from_Haas()
 
 
--- haasNewAdapter.py
+++ haasNewAdapter.py
@@ -55,13 +55,33 @@ def clear_thread_list():
 
 def readData(ser, HAASCode):
     try:
+        #print("Inside readData loop...")
         ser.write(bytes("?Q600 " + HAASCode + "\r\n", "ascii"))
+        #print("Query ?Q600 made...")
+        count = 0
+        retry_limit = 10  # Limit of retries
+        retries = 0       # Initial retry count
+
         while True:
+            count += 1
+            #print("Inside while loop")
             value = ser.readline().decode("utf-8").strip()
+            #print(f"Value decoded {count} and value is [{value}]")
+
             if len(value) > 4:
+                #print("broke")
                 break
+            
+            # Increment retries count and check if limit is reached
+            retries += 1
+            if retries >= retry_limit:
+                print("Retry limit reached.")
+                return "Unavailable"
+
         value = value.split(",")[2].strip()
+        #print(f"Value was split {count} and splitValue is [{value}]")
         value = value.replace(chr(23), '')
+        #print(f"Value was replaced {count} and replaceValue is [{value}]")
     except Exception as ex:
         print(ex)
         value = 'Unavailable'
@@ -186,7 +206,7 @@ def fetch_from_Haas():
 
                 # Final data purge
                 combined_output = '\r\n' + datetime.datetime.now().isoformat() + 'Z' + outString
-                print(f"------> {combined_output}")
+                print(f"---> {combined_output}")
 
             # Error catch
             except Exception as ex:
